d446c2701336d8eb07a508e3bba155919e673c84
Ben Webb <ben@salilab.org>
2014-05-14 14:02:37 -0700

    SWIG still needs the deprecated headers (for now).

--- make_all_header.py
+++ make_all_header.py
@@ -30,13 +30,27 @@ output.append("#define %s" % guard)
 for h in sys.argv[3:]:
     if not h.endswith(".h"):
         pat = os.path.join(h, "*.h")
-        allh = sorted(glob.glob(pat))
-        allh = [x for x in allh if 'DEPRECATED_HEADER' not in open(x).read()]
+        orig_h = sorted(glob.glob(pat))
+        allh = []
+        deprecated_allh = []
+        for h in orig_h:
+            if 'DEPRECATED_HEADER' in open(h).read():
+                deprecated_allh.append(h)
+            else:
+                allh.append(h)
     else:
+        deprecated_allh = []
         allh = [h]
     for g in allh:
         name = os.path.split(g)[1]
         output.append("#include <%s/" % sys.argv[2] + name + ">")
+    if deprecated_allh:
+        # SWIG needs all headers (for now)
+        output.append("#ifdef IMP_SWIG_WRAPPER")
+        for g in deprecated_allh:
+            name = os.path.split(g)[1]
+            output.append("#include <%s/" % sys.argv[2] + name + ">")
+        output.append("#endif")
 
 output.append("#endif /* %s */" % guard)
 python_tools.rewrite(sys.argv[1], "\n".join(output))
