06cfcb7d9d89082661ae4a6f4a4662c142efac27
Ben Webb <ben@salilab.org>
2016-03-14 16:24:43 -0700

    Enable collection of Python coverage info.

--- test/utils.py
+++ test/utils.py
@@ -1,5 +1,7 @@
 import tempfile
 import shutil
+import os
+import sys
 
 class TempDir(object):
     def __enter__(self):
@@ -15,3 +17,27 @@ def write_file(fname, content):
 def read_file(fname):
     with open(fname, "r") as fh:
         return fh.read()
+
+if 'coverage' in sys.modules:
+    import atexit
+    # Collect coverage information from subprocesses
+    __site_tmpdir = tempfile.mkdtemp()
+    with open(os.path.join(__site_tmpdir, 'sitecustomize.py'), 'w') as fh:
+        fh.write("""
+import coverage
+import atexit
+
+_cov = coverage.coverage(branch=True, data_suffix=True, auto_data=True,
+                         data_file='%s/.coverage')
+_cov.start()
+
+def _coverage_cleanup(c):
+    c.stop()
+atexit.register(_coverage_cleanup, _cov)
+""" % os.getcwd())
+
+    os.environ['PYTHONPATH'] = __site_tmpdir
+
+    def __cleanup(d):
+        shutil.rmtree(d, ignore_errors=True)
+    atexit.register(__cleanup, __site_tmpdir)
